b8d3a746d70cfd775fcc4ef7982c7bfdc249348c
janoPig <jan.pigos@gmail.com>
2022-09-01 11:51:48 +0200

    fix HROCH regressor

--- competition-2022/experiment/methods/HROCH/regressor.py
+++ competition-2022/experiment/methods/HROCH/regressor.py
@@ -7,9 +7,7 @@ est = Hroch()
 def model(est, X=None):
     if X is None or not hasattr(X, 'columns'):
         return str(est.sexpr)
-
     model_str = str(est.sexpr)
-    print('model_str:',model_str)
     mapping = {'x_'+str(i): k for i, k in enumerate(X.columns)}
     new_model = model_str
     for k, v in reversed(mapping.items()):
@@ -20,14 +18,17 @@ def model(est, X=None):
 
 def my_pre_train_fn(est, X, y):
     try:
-        est.numThreads = os.environ['OMP_NUM_THREADS']
+        est.numThreads = int(os.environ['OMP_NUM_THREADS'])
     except Exception as e:
         print(e)
 
+    # run default 5 seconds
+    '''
     max_time = 3600 - 60  # 60 second of slack
     if len(X) > 1000:
         max_time = 36000 - 60  # 60 second of slack
     est.timeLimit = max_time * 1000
+    '''
 
     print(
         f'my_pre_train_fn: timeLimit={est.timeLimit} numThreads={est.numThreads}')
@@ -38,5 +39,6 @@ eval_kwargs = dict(
     pre_train=my_pre_train_fn,
     scale_x=False,  # avoid poisonous scaling
     scale_y=False,
-    test_params=dict(timeLimit=1000)
+    test_params={},
+    DataFrame=False
 )
