dd8b180a7ef15889d7b9185a8a834cd40011bebd
janoPig <jan.pigos@gmail.com>
2023-01-14 15:58:29 +0100

    fixup! add regressor variants, result overwrited

--- experiment/methods/PHCRegressor1.py
+++ experiment/methods/PHCRegressor1.py
@@ -1,11 +1,11 @@
-from HROCH import PHCRegressor as PHCRegressor0
+from HROCH import PHCRegressor as PHCRegressor1
 from symbolic_utils import complexity as cplx
 import sympy as sp
 
 
 # Create the pipeline for the model
 eval_kwargs = {'scale_x': False, 'scale_y': False}
-est = PHCRegressor0(time_limit=10.0, num_threads=1)
+est = PHCRegressor1(time_limit=10.0, num_threads=1)
 
 
 def model(est):
