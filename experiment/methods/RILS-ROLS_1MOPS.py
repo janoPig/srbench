285c3b27e43a1e8e9ccf77b1ac4824ef8d8f8a5d
Jano <jan.pigos@gmail.com>
2023-12-11 09:47:34 +0100

    Expreiment with 1M operaations

---
+++ experiment/methods/RILS-ROLS_1MOPS.py
@@ -0,0 +1,16 @@
+from rils_rols.rils_rols import RILSROLSRegressor as RILSROLSRegressor_1MOPS
+from symbolic_utils import complexity as cplx
+import sympy as sp
+
+
+# Create the pipeline for the model
+eval_kwargs = {'scale_x': False, 'scale_y': False}
+est = RILSROLSRegressor_1MOPS(max_seconds=60*60, max_fit_calls=1000000, max_complexity=50, sample_size=1.0)
+
+
+def model(est):
+    return str(est.model_string())
+
+
+def complexity(est):
+    return cplx(sp.parse_expr(str(est.model_string())))
