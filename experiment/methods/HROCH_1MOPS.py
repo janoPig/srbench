78cbd479e78e78a07d99c28733c826b67f57e301
Jan Pigos <jan.pigos@gmail.com>
2024-09-01 14:54:47 +0200

    fix names

--- experiment/methods/HROCH_1MOPS.py
+++ experiment/methods/HROCH_1MOPS.py
@@ -1,4 +1,4 @@
-from HROCH import SymbolicRegressor as HROCH_1M_EQ
+from HROCH import SymbolicRegressor as HROCH_1MOPS
 from symbolic_utils import complexity as cplx
 import sympy as sp
 
@@ -6,7 +6,7 @@ import sympy as sp
 # Create the pipeline for the model
 eval_kwargs = {'scale_x': False, 'scale_y': False}
 # evaluate 1M equations(each iteration try 15 mutations)
-est = HROCH_1M_EQ(time_limit=0, iter_limit=1000000//15, num_threads=1)
+est = HROCH_1MOPS(time_limit=0, iter_limit=1000000//15, num_threads=1)
 
 
 def model(est):
